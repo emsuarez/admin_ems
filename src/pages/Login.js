d8e538a849ebea2862acf18a37f6fc4153cddd51
ChrisPaulRTV <creinoso7982@gmail.com>
2022-07-28 10:43:51 -0500

    [MOD] Cambios en diseño Login.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -46,24 +46,26 @@ const Login = props => {
   }
 
   return (
-    <div className='bg-white  flex h-screen w-screen justify-center overflow-hidden'>
+    <div className='bg-white flex flex-col h-screen justify-center'>
       <RedirectWithLogin />
       {/* LOGIN CARD */}
-      <div className='flex bg-white shadow-md border-2 lg:px-52 -mt-32 md:mt-0   flex-col justify-center'>
-        <div className=''>
-          <img src={logo} className='h-24 flex ml-28 -mt-1' />
+      <div className='flex flex-col justify-center items-center'>
+        <div className='flex justify-center'>
+          <img src={logo} alt='logo' className='h-32' />
         </div>
 
-        <div className='items-center mx-8'>
+        <div className='flex flex-col items-center'>
           <div className='flex space-x-2'>
-            <h1 className='text-4xl font-semibold'>¡Bienvenido!</h1>
-            <ICONS.SparklesIconS className='h-10 pr-20 mt-0 md:mt-1 text-blue-600' />
+            <h1 className='text-3xl font-bold'>
+              ¡Bienvenido bitácora EMSECOR!
+            </h1>
+            {/* <ICONS.SparklesIconS className='h-10 pr-20 mt-0 md:mt-1 text-blue-600' /> */}
           </div>
           {/* EMAIL */}
           <div className='mt-10'>
             <p className='font-medium'>Usuario</p>
             <input
-              className='border-[1px] border-neutral-300 pl-2 rounded-md py-2 w-96 focus:border-blue-500 outline-none'
+              className='border-[1px] border-neutral-300 pl-2 rounded-md py-2 w-80 focus:border-blue-500 outline-none'
               onChange={item => setUsername(item.target.value)}
             />
           </div>
@@ -72,29 +74,29 @@ const Login = props => {
           <div className='mt-10'>
             <p className='font-medium'>Contraseña</p>
             <input
-              className='border-[1px] border-neutral-300 pl-2 rounded-md py-2 w-96 focus:border-blue-500 outline-none'
+              className='border-[1px] border-neutral-300 pl-2 rounded-md py-2 w-80 focus:border-blue-500 outline-none'
               type='password'
               onChange={item => setPassword(item.target.value)}
             />
           </div>
 
-          <div className='flex justify-between mt-10 border-b-[1px] pb-8'>
-            <p className='ml-2 mt-2 underline text-lg hover:cursor-pointer hover:no-underline text-gray-600'>
+          <div className='flex mt-10 space-x-2 border-b-[1px] pb-4 justify-start'>
+            <p className='mt-2 hover:underline text-lg hover:cursor-pointer text-gray-600'>
               ¿Olvidé mi contraseña?
             </p>
-            <h4
+            <button
               onClick={() => handleLogin()}
-              className='bg-blue-600 hover:bg-blue-500 text-white hover:cursor-pointer font-medium text-lg p-2 rounded-lg'
+              className='bg-blue-900 hover:bg-blue-800 text-white hover:cursor-pointer font-medium text-xl px-4 rounded-md'
             >
               Ingresar{' '}
-            </h4>
+            </button>
           </div>
 
-          <div className='flex space-x-2 mt-10'>
-            <p className='text-gray-600'>¿No tienes una cuenta?</p>
+          <div className='flex space-x-2 mt-5'>
+            <p className='text-gray-600 text-lg'>¿No tienes una cuenta?</p>
             <p
               onClick={() => navigate('/register')}
-              className='text-blue-600 font-medium hover:cursor-pointer hover:text-blue-700'
+              className='text-blue-800 font-medium hover:cursor-pointer hover:text-blue-700'
             >
               Regístrate
             </p>
