7e8d66504add66ed8b76180db6f915385f762c9f
ChrisPaulRTV <creinoso7982@gmail.com>
2022-08-02 17:49:11 -0500

    [ADD] Se agrega eye icon para mostrar y ocultar contraseña

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,31 +3,14 @@ import logo from '../assets/logo.png'
 import { useNavigate } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { setToast, UserLogin } from '../store/actions'
-import { RedirectWithLogin } from '../components'
+import { ICONS, RedirectWithLogin } from '../components'
 
 const Login = props => {
   const [username, setUsername] = useState()
   const [password, setPassword] = useState()
+  const [showPassword, setShowPassword] = useState(true)
 
   let navigate = useNavigate()
-
-  var token
-  var tipo
-
-  const handleLogin = async () => {
-    const obj = {
-      username,
-      password,
-    }
-    if (username && password) {
-      await props.UserLogin(obj)
-    } else {
-      await props.setToast(
-        'error',
-        'Uno de los campos del formulario estan vacios.'
-      )
-    }
-  }
 
   useEffect(() => {
     token = window.localStorage.getItem('token')
@@ -50,6 +33,28 @@ const Login = props => {
       }
     }
   }, [])
+
+  var token
+  var tipo
+
+  const handleLogin = async () => {
+    const obj = {
+      username,
+      password,
+    }
+    if (username && password) {
+      await props.UserLogin(obj)
+    } else {
+      await props.setToast(
+        'error',
+        'Uno de los campos del formulario estan vacios.'
+      )
+    }
+  }
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword)
+  }
 
   return (
     <div className='bg-white flex h-screen justify-center'>
@@ -75,13 +80,46 @@ const Login = props => {
           </div>
 
           {/* PASSWORD */}
-          <div className='mt-10'>
+          <div className='mt-10 relative'>
             <p className='font-medium'>Contraseña</p>
             <input
-              className='border-[1px] border-neutral-300 pl-2 rounded-md py-2 w-80 focus:border-blue-800 outline-none'
-              type='password'
+              className='border-[1px] border-neutral-300 pl-2 rounded-md py-2 w-80 focus:border-blue-800 outline-none w-full'
+              type={showPassword ? 'password' : 'text'}
               onChange={item => setPassword(item.target.value)}
             />
+            <span class='absolute inset-y-0 flex items-center pt-3 ml-72'>
+              <button onClick={togglePassword} className='z-0 pt-2'>
+                {!showPassword ? (
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='h-5 w-5'
+                    viewBox='0 0 20 20'
+                    fill='currentColor'
+                  >
+                    <path d='M10 12a2 2 0 100-4 2 2 0 000 4z' />
+                    <path
+                      fillRule='evenodd'
+                      d='M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z'
+                      clipRule='evenodd'
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='h-5 w-5'
+                    viewBox='0 0 20 20'
+                    fill='currentColor'
+                  >
+                    <path
+                      fillRule='evenodd'
+                      d='M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z'
+                      clipRule='evenodd'
+                    />
+                    <path d='M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z' />
+                  </svg>
+                )}
+              </button>
+            </span>
           </div>
 
           <div className='flex mt-10 space-x-2 border-b-[1px] pb-4 justify-start'>
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -18,6 +18,8 @@ const Registration = props => {
   const [confirmPassword, setConfirmPassword] = useState()
   const [tipo, setTipo] = useState(0)
 
+  const [showPassword, setShowPassword] = useState(true)
+
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -29,6 +31,10 @@ const Registration = props => {
   const roles = useSelector(state => state.auth.roles)
 
   let navigate = useNavigate()
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword)
+  }
 
   const handleRegister = async dispatch => {
     const obj = {
@@ -106,14 +112,47 @@ const Registration = props => {
             </div>
 
             {/* Contraseña */}
-            <div className='mt-5'>
+            <div className='mt-5 relative'>
               <p className='font-medium'>Contraseña</p>
               <input
-                className='border-[1px] border-neutral-300 pl-2 rounded-md py-2 w-80 focus:border-blue-800 outline-none'
-                type='password'
+                className='border-[1px] border-neutral-300 pl-2 rounded-md py-2 w-80 focus:border-blue-800 outline-none w-full'
+                type={showPassword ? 'password' : 'text'}
                 value={contraseña}
                 onChange={item => setContraseña(item.target.value)}
               />
+              <span class='absolute inset-y-0 flex items-center pt-3 ml-72'>
+                <button onClick={togglePassword} className='pt-2'>
+                  {!showPassword ? (
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      className='h-5 w-5'
+                      viewBox='0 0 20 20'
+                      fill='currentColor'
+                    >
+                      <path d='M10 12a2 2 0 100-4 2 2 0 000 4z' />
+                      <path
+                        fillRule='evenodd'
+                        d='M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z'
+                        clipRule='evenodd'
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      className='h-5 w-5'
+                      viewBox='0 0 20 20'
+                      fill='currentColor'
+                    >
+                      <path
+                        fillRule='evenodd'
+                        d='M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z'
+                        clipRule='evenodd'
+                      />
+                      <path d='M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z' />
+                    </svg>
+                  )}
+                </button>
+              </span>
             </div>
 
             {/* Repite la Contraseña */}
