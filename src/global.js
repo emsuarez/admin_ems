7daa9ba59ca8c3044b2b20e6388c387484ede0c6
Enver Suarez <enver.onze@gmail.com>
2024-02-26 13:35:09 -0500

    [Mod] cambios para q funcione en local

--- src/global.js
+++ src/global.js
@@ -1,2 +1,4 @@
-global.baseUrl = 'https://cloudbitakor.com/api/1.0'
-global.urlMedia = 'https://cloudbitakor.com'
+//global.baseUrl = 'https://cloudbitakor.com/api/1.0'
+//global.urlMedia = 'https://cloudbitakor.com'
+global.baseUrl = 'http://127.0.0.1:8000/api/1.0'
+global.urlMedia = 'http://127.0.0.1:8000'
